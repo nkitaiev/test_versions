8851b82e3b1960aa8595e03719af8c38bdf3db59
Nikita Kitaiev <n.kitaiev@smissltd.com>
2023-08-10 16:08:07 +0300

    chore(release): asdfasdf

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    ignores: [(message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message)],
+    rules: {
+        'body-max-line-length': [0, 'always'],
+        'footer-max-line-length': [0, 'always']
+    }
+    // ignores: [(message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message)],
 }
